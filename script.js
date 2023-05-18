ed4a8d4c86a42e57a9119e05c2dd7d1b67f114c7
Janu Kumar Singh <119485859+jks6404@users.noreply.github.com>
2023-05-18 17:24:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-function calculateMinCost() {
+function calculateMinCost(ropeLengths) {
   //your code here
+  let cost = 0;
+  let sortedLengths = ropeLengths.sort((a, b) => a - b); // sort the rope lengths in ascending order
+  
+  while (sortedLengths.length > 1) {
+    // take the two smallest ropes
+    let smallestRope = sortedLengths.shift();
+    let secondSmallestRope = sortedLengths.shift();
+    
+    // add their lengths and insert the new rope back into the array
+    let newRopeLength = smallestRope + secondSmallestRope;
+    cost += newRopeLength;
+    sortedLengths.push(newRopeLength);
+    sortedLengths.sort((a, b) => a - b); // re-sort the array
+  }
+  
+  return cost;
   
   
   
